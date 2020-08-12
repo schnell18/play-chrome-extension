8bcf88e0f08b0929750640ac2078e0d4534aaf0c
Justin Zhang <schnell18@gmail.com>
2020-08-12 23:24:54 +0800

    Add color changing extension to work w/ page

---
+++ pagefontstyle/content.js
@@ -0,0 +1,6 @@
+chrome.runtime.sendMessage({todo: "showPageAction"})
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.todo == "changeColor") {
+        $('.api').css('color', request.clickedColor)
+    }
+})
